234102758b9f8a9f318a25260ac35ac7cdac55fc
7r15 <287508559+7r15@users.noreply.github.com>
2026-07-05 22:11:46 -0400

    Add rate-this-app control to the top bar

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -12,6 +12,7 @@ import { useColors } from '../../theme/ColorTokensContext';
 import { tokens } from '../../theme/tokens';
 import { themeAtom } from '../../state/atoms';
 import { EnumTheme } from '../../types';
+import { RatingControl } from './RatingControl';
 
 const APP_QDN_NAME = 'Chain';
 
@@ -121,7 +122,9 @@ export function TopBar() {
         />
       </Box>
 
-      <Tooltip title={isFollowed ? 'Unfollow' : 'Follow'} placement="bottom">
+      <RatingControl qdnName={APP_QDN_NAME} />
+
+      <Tooltip title={isFollowed ? 'Stop following this app' : 'Follow this app'} placement="bottom">
         <IconButton
           size="small"
           onClick={() => void handleToggleFollow()}
